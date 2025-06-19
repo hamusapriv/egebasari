fcb8549c4cdab9e41de5b21bfd6fc700ce8a11a2
hamusapriv <trusted.acc2023@outlook.com>
2025-06-19 03:24:04 +0300

    Add responsive Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Navbar />
       <div>hello ankara</div>
     </>
   );
