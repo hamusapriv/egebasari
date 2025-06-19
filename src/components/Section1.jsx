cce2484077d2bada6926e8ebebd629c9799b491a
Hamusa96 <hamza@edgevideo.com>
2025-06-20 01:49:30 +0300

    section 1 header

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Section1 = () => {
-  return <section className="section-1">Section 1</section>;
+  return (
+    <section className="section-1">
+      <h2>Bizimle Tanışın</h2>
+    </section>
+  );
 };
 
 export default Section1;
